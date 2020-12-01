3b8c7a8da188d7e385cf766be8f5dfef3537136e
Anderson Levasseur <anderson.levasseur@gmail.com>
2020-12-01 12:23:35 -0500

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+// Calculator
+
+// ALLCALCULATORSTUFHERE
+let currentValue
+let number = function(number) {
+	return currentValue = number
+}
+let numberOperator = function() {
+
+}
+let equals = function() {
+	return currentValue
+}
