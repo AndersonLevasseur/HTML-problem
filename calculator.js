3ab991ce7b85895de0d958c709db616e8662aec8
Anderson Levasseur <anderson.levasseur@gmail.com>
2020-12-08 14:19:13 -0500

    12/8 tinkered, added data values

--- calculator.js
+++ calculator.js
@@ -1,6 +1,31 @@
 // Calculator
 let display = document.getElementById("display");
 let buttons = document.getElementsByTagName("button");
+
+let buttonHandler = function(element) {
+	display.in1nerHTML = display.innerHTML.concat(element.dataset.value, display.innerHTML); 
+}
+
+for(let button of buttons) {
+	button.addEventListener("click", buttonHandler, false);
+}
+
+console.log(buttons.dataset.value)
+
+/* 
+let display = document.getElementById("display");
+let buttons = document.getElementsByTagName("button");
+
+buttons.onclick(display.changevalue(data-value OF SPECIFIC BUTTON))
+*/ 
+/*
+let display = document.getElementById("display");
+let buttons = document.getElementsByTagName("button");
+
+NOTES
+
+dataset can't read html collections
+
 let buttonHandler = function(element) {
 	display.innerHTML = display.innerHTML.concat(element.dataset.value); 
 }
@@ -8,3 +33,4 @@ let buttonHandler = function(element) {
 for(let button of buttons) {
 	button.addEventListener("click", buttonHandler);
 }
+*/
